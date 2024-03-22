6279fbf5c35b54af5e52895ba8712ec71dfb1710
maitha1-nule <nuleahmed@gmail.com>
2024-03-23 01:27:21 +0300

    In this file, i've created a script which used puppet to insyall flask from pip3 and gve it a specific version of 2.1.0

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,9 @@
-#!/usr/bin/pup
 ## Using puppet intall flask from pip3 and version must be 2.1.0
+package { 'python3-pip':
+  ensure => installed,
+}
 package { 'flask':
 ensure   => '2.1.0',
 provider => 'pip3'
+require  => Package['python3-pip'],
 }
