d01b005f2d2dab924ee3a14e0f5213d0cf07b52a
maitha1-nule <nuleahmed@gmail.com>
2024-03-23 01:47:08 +0300

    in thi file make changes

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,9 @@
-#!/usr/bin/pup
 ## Using puppet intall flask from pip3 and version must be 2.1.0
-package { 'python3-pip':
-  ensure => installed,
-}
-package { 'flask':
+package { 'Flask':
 ensure   => '2.1.0',
-provider => 'pip3',
-require  => Package['python3-pip'],
+provider => 'pip3'
+}
+package { 'Werkzeug':
+ensure   => '2.1.1',
+provider => 'pip3'
 }
