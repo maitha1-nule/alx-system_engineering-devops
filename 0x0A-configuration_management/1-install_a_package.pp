68f11e55030915b9fa3fe3198eeaa73b87b1599f
maitha1-nule <nuleahmed@gmail.com>
2024-03-23 01:19:12 +0300

    In this file, i've created a script which used puppet to insyall flask from pip3 and gve it a specific version of 2.1.0

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
+#!/usr/bin/pup
 ## Using puppet intall flask from pip3 and version must be 2.1.0
 package { 'flask':
 ensure   => '2.1.0',
-provider => 'pip3',
+provider => 'pip3'
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 ## A script using puppet that creates a manifest that kills a process named killmenow
-exec { 'kill_process':
-command => '/usr/bin/pkil -f killmenow',
-onlyif  => '/usr/bin/pgrep -f killmenow',
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell'
 }
